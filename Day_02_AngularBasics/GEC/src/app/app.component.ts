ea73ccdb5626d2f7c31de0702444b174c143a20f
Harsh Vadher <153319594+Harshvadher05@users.noreply.github.com>
2025-07-09 23:54:54 +0530

    day 2 work

--- Day_02_AngularBasics/GEC/src/app/app.component.ts
+++ Day_02_AngularBasics/GEC/src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';  //use for routing purpose
 import { HeaderComponent } from './header/header.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, RouterOutlet],
+  imports: [HeaderComponent, CommonModule, FormsModule,LoginComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'GEC';
+  name = 'College';
+  visibility = false;
+
+  list = ['Angular', 'React', 'Vue', 'Svelte', 'Ember'];
+
+  store = '';
+
+  listener() {
+    this.visibility = !this.visibility;
+    console.log(this.store);
+  }
 }
---
+++ Day_02_AngularBasics/GEC/src/app/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  username: string = '';
+  password: string = '';
+
+  onLogin() {
+    console.log('Username:', this.username);
+    console.log('Password:', this.password);
+  }
+}
